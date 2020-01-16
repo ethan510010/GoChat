34d6594818678307804d8a2b3b27bf290e84fdc9
EthanLin <ethan510010@gmail.com>
2020-01-16 14:35:02 +0800

    complete fundamental login function

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -4,5 +4,11 @@ notMemberTag.addEventListener('click', function(event) {
   const signupInfoArea = document.querySelector('.signup_info');
   const displayValue = showSignupArea === true ? 'none' : 'block';
   signupInfoArea.style.display = displayValue;
+
+  const signInArea = document.querySelector('.sign_in_area');
+  
+  const signInHidden = showSignupArea === true ? 'block' : 'none';
+  signInArea.style.display = signInHidden;
+
   showSignupArea = !showSignupArea
 })
