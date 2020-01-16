6432f979c7dbe900373c35adde4bd9442e6b0d99
EthanLin <ethan510010@gmail.com>
2020-01-16 14:08:53 +0800

    signup area logic completed

---
+++ public/javascripts/home.js
@@ -0,0 +1,8 @@
+let showSignupArea = false;
+const notMemberTag = document.querySelector('#not_member_hint');
+notMemberTag.addEventListener('click', function(event) {
+  const signupInfoArea = document.querySelector('.signup_info');
+  const displayValue = showSignupArea === true ? 'none' : 'block';
+  signupInfoArea.style.display = displayValue;
+  showSignupArea = !showSignupArea
+})
