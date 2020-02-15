fc4f031bada92df13972ed64622cb325818a9ac1
EthanLin <ethan510010@gmail.com>
2020-02-15 11:28:45 +0800

    handle handup video logic

--- public/javascripts/hangup.js
+++ public/javascripts/hangup.js
@@ -1,6 +1,6 @@
 const hangupCallBtn = document.getElementById('hangup');
 hangupCallBtn.addEventListener('click', function() {
-  // 代表是由發起視訊的人掛斷
+  // 代表是由發起視訊的人 (視訊發起者) 掛斷
   if (!receiveCallId) {
     const allCalls = Object.values(callConnections);
     console.log('掛斷的 connenctions', callConnections);
@@ -13,12 +13,40 @@ hangupCallBtn.addEventListener('click', function() {
     isPlayingLocalVideo = false;
     // 把視訊畫面關掉
     videoDisplayDiv.style.display = 'none';
+    // 移除
   } else {
-    // 代表是看到視訊的觸發掛斷的
-    callConnections[receiveCallId].close();
-    delete callConnections[receiveCallId];
+    // 代表是看到視訊 (視訊接收者) 的觸發掛斷的
+    if (callConnections[receiveCallId]) {
+      callConnections[receiveCallId].close();
+      delete callConnections[receiveCallId];  
+    }
     // 代表已經沒有在看遠端視訊了
     isWatchingRemoteVideo = false;
     videoDisplayDiv.style.display = 'none';
   }
+  resetVideo();
 })
+
+function resetVideo() {
+  const mainAreaTag = document.querySelector('.videoPopup .main_area');
+  const localVideoTag = document.getElementById('localVideo');
+  const remoteVideoTag = document.getElementById('remoteVideo');
+  if (localVideoTag) {
+    mainAreaTag.removeChild(localVideoTag);
+  }
+  if (remoteVideoTag) {
+    mainAreaTag.removeChild(remoteVideoTag);
+  }
+  const resetLocalVideoTag = document.createElement('video');
+  resetLocalVideoTag.setAttribute('id', 'localVideo');
+  resetLocalVideoTag.autoplay = true;
+  resetLocalVideoTag.playsinline = true;
+  resetLocalVideoTag.style.height = 'calc((100% - 40px)/2)';
+  const resetRemoteVideoTag = document.createElement('video');
+  resetRemoteVideoTag.setAttribute('id', 'remoteVideo');
+  resetRemoteVideoTag.autoplay = true;
+  resetRemoteVideoTag.playsinline = true;
+  resetRemoteVideoTag.style.height = 'calc((100% - 40px)/2)';
+  mainAreaTag.prepend(resetRemoteVideoTag);
+  mainAreaTag.prepend(resetLocalVideoTag);
+}
