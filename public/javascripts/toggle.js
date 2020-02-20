c744a722f61587bb7b60033c73b54e7e245bfbc4
EthanLin <ethan510010@gmail.com>
2020-02-20 16:35:19 +0800

    optimize UI shrink

--- public/javascripts/toggle.js
+++ public/javascripts/toggle.js
@@ -2,6 +2,9 @@ let toggleShow = false;
 const toggleSwitcher = document.querySelector('.header img');
 toggleSwitcher.addEventListener('click', function() {
   toggleShow = !toggleShow;
+  const chatContentWidth = toggleShow ? 'calc(75% - 300px)' : '75%';
+  const chatContentDiv = document.querySelector('.chat_content');
+  chatContentDiv.style.width = chatContentWidth;
   const toggleList = document.getElementById('toggle_list');
   const toggleRightPosition = toggleShow ? 0 : '-300px'
   toggleList.style.right = toggleRightPosition;
